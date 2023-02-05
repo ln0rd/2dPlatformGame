cbcddd9fa60079ff7c4ca6bde4113b591b0a6fac
ln0rd <leonrd.lbf@gmail.com>
2023-02-04 22:02:38 -0300

    feat: execute jumping

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,4 @@
-//using System.Threading.Tasks.Dataflow;
+using System.Transactions;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,9 +7,9 @@ public class Player : MonoBehaviour
 {
 
     public float speed;
-    //public int jumpForce;
-    //public int health;
-    //public Transform groundCheck;
+    public int jumpForce;
+    public int health;
+    public Transform groundCheck;
 
     private bool invulnarable = false;
     private bool grounded = false;
@@ -24,6 +24,7 @@ public class Player : MonoBehaviour
     // uma unica vez ao iniciar
     void Start() {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        // e o objeto de fisica e quem sofre as ações da gravidade
         rigidbody2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
@@ -31,7 +32,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     // se houver 30 fps ele executara 30 vezes por segundo
     void Update() {
-        
+        // ele cria uma linha imaginaria para que vc saiba se ele está encostando no chao, ultimo parametro é a camada que está tocado
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            jumping = true;
+        }
+
     }
 
     // faz o mesmo que a Update mas indicado para RigidBodys
@@ -39,25 +47,35 @@ public class Player : MonoBehaviour
         // quando vai para a direita o valor e positivo, esquerda e negativo
         float move = Input.GetAxis("Horizontal");
 
-        movePlayer(move);
-        turnLeftOrRight(move);
+        ExecuteMove(move);
+        TurnLeftOrRight(move);
+        ExecuteJumping();
     }
 
-    float movePlayer(float move) {
+    float ExecuteMove(float move) {
         rigidbody2D.velocity = new Vector2(move * speed, rigidbody2D.velocity.y);
         return move;
     }
 
-    void turnLeftOrRight(float move) {
+    void TurnLeftOrRight(float move) {
         if(move < 0f && facingRight || move > 0f && !facingRight) {
-            Flip();
-        };
+            ExecuteFlip();
+        }
     }
 
-    void Flip() {
+    void ExecuteFlip() {
         facingRight = !facingRight;
         transform.localScale = new Vector3(
             -transform.localScale.x, transform.localScale.y, transform.localScale.z);
         
+    }
+
+    void ExecuteJumping() {
+        if (jumping)
+        {
+            rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+            // para nao pular novamente e o personagem nao sair voando
+            jumping = false;
+        }
     }
 }
